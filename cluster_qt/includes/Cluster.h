3acd76f09c373266428c48ac9c09ada09ad4b233
computer_afjp <computer_afjp@gmail.com>
2024-12-04 12:53:47 +0000

    cluster.h created for main.cpp, cleaned code and deleted commentary on main

---
+++ cluster_qt/includes/Cluster.h
@@ -0,0 +1,17 @@
+#ifndef CLUSTER_H
+#define CLUSTER_H
+
+#include <QApplication>
+#include <QHBoxLayout>
+#include <QLabel>
+#include <QKeyEvent>
+#include <cmath>
+
+#include "ArrowSymbolWidget.h"
+#include "SpeedometerWidget.h"
+#include "FanSpeedWidget.h"
+#include "BatteryWidget.h"
+#include "TopBar.h"
+#include "Blinkers.h"
+
+#endif
--- cluster_qt/main.cpp
+++ cluster_qt/main.cpp
@@ -1,55 +1,30 @@
-#include <QApplication>
-#include <QHBoxLayout>
-#include <QLabel>
-#include <QKeyEvent>
-#include <cmath> // For sin and cos
-
-#include "includes/ArrowSymbolWidget.h"
-#include "includes/SpeedometerWidget.h"
-#include "includes/FanSpeedWidget.h"
-#include "includes/BatteryWidget.h"
-#include "includes/TopBar.h"
-#include "includes/Blinkers.h"
+#include "includes/Cluster.h"
 
 int main(int argc, char* argv[])
 {
-    QApplication app(argc, argv);
+    QApplication        app(argc, argv);
+    QWidget             mainWindow;
 
-    // Create the main window
-    QWidget mainWindow;
+    TopBar*             topBar = new TopBar(&mainWindow);
+    Blinkers*           leftB = new Blinkers(&mainWindow, "left", "off");
+    Blinkers*           rightB = new Blinkers(&mainWindow, "right", "on");
+    SpeedometerWidget*  speedometer1 = new SpeedometerWidget(&mainWindow);
+    FanSpeedWidget*     fanspeed = new FanSpeedWidget(&mainWindow);
+    BatteryWidget*      battery2 = new BatteryWidget(&mainWindow);
+    ArrowSymbolWidget*  arrowSymbol = new ArrowSymbolWidget(&mainWindow, "zero");
 
-    // Create the TopBar
-    TopBar* topBar = new TopBar(&mainWindow);
-
-    Blinkers* leftB = new Blinkers(&mainWindow, "left", "off");
-    Blinkers* rightB = new Blinkers(&mainWindow, "right", "on");
-
-
-    SpeedometerWidget* speedometer1 = new SpeedometerWidget(&mainWindow);
-    FanSpeedWidget* fanspeed = new FanSpeedWidget(&mainWindow);
-    BatteryWidget* battery2 = new BatteryWidget(&mainWindow);
-
-    ArrowSymbolWidget* arrowSymbol = new ArrowSymbolWidget(&mainWindow, "zero");
+    QVBoxLayout* mainLayout = new QVBoxLayout();
+    QHBoxLayout* contentLayout = new QHBoxLayout();
+    
     arrowSymbol->setFixedSize(400, 400);
-
-
-
     speedometer1->resize(900, 900);
     speedometer1->setFixedSize(400, 400);
     battery2->resize(900, 900);
     battery2->setFixedSize(400, 400);
 
- 
-    // Create layouts
-    QVBoxLayout* mainLayout = new QVBoxLayout(); // Main vertical layout
-    QHBoxLayout* contentLayout = new QHBoxLayout(); // Horizontal layout for speedometers
+    mainLayout->addWidget(topBar, 0, Qt::AlignCenter);
+    mainLayout->addWidget(fanspeed, 0);
 
-    // Add the TopBar to the main layout
-    mainLayout->addWidget(topBar, 0, Qt::AlignCenter); // Add the TopBar widget at the top
-    mainLayout->addWidget(fanspeed, 0); // Add the TopBar widget at the top
-
-
-    // Add the horizontal layout to the main vertical layout
     contentLayout->addWidget(leftB, 1);
     contentLayout->addWidget(speedometer1, 1);
     contentLayout->addWidget(arrowSymbol, 1);
@@ -57,21 +32,18 @@ int main(int argc, char* argv[])
     contentLayout->addWidget(rightB, 1);
 
     mainLayout->addLayout(contentLayout, 1);
-    // Set the layout for the main window
     mainWindow.setLayout(mainLayout);
 
-    // Resize the main window and show it
-    mainWindow.resize(1468, 768); // Adjusted to accommodate the image and speedometers
-    mainWindow.setStyleSheet("background-color: #0D2126"); // Replace Qt::blue with your desired color
+    mainWindow.resize(1468, 768);
+    mainWindow.setStyleSheet("background-color: #0D2126");
     mainWindow.show();
 
-    // Simulate changes to TopBar images
     QTimer::singleShot(2000, [&]() { topBar->setImageState(0, true); });
     QTimer::singleShot(3000, [&]() { topBar->setImageState(1, true); });
     QTimer::singleShot(4000, [&]() { topBar->setImageState(2, true); });
     QTimer::singleShot(6000, [&]() { topBar->setImageState(4, true); });
     QTimer::singleShot(7000, [&]() { topBar->setImageState(3, true); });
-    QTimer::singleShot(8000, [&]() { topBar->setImageState(5, true); }); // Revert to default
+    QTimer::singleShot(8000, [&]() { topBar->setImageState(5, true); });
 
     return app.exec();
 }
