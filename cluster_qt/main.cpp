451ed9fa16411eb92fe0bdf5ad5985e80f92e86d
computer_afjp <computer_afjp@gmail.com>
2024-12-12 17:37:36 +0000

    camera widget improved

--- cluster_qt/main.cpp
+++ cluster_qt/main.cpp
@@ -4,19 +4,17 @@ int main(int argc, char* argv[])
 {
     QApplication        app(argc, argv);
 
-    // Set a main window where the pages will be included
-    QWidget *mainWindow = new QWidget;
 
-    // Set the pages
+    QWidget *mainWindow = new QWidget;
     QWidget *mainPage = new QWidget;
     QWidget *secondPage = new QWidget;
 
-    // Set a stack of Widgets to store the pages
+
     QStackedWidget *stackedWidget = new QStackedWidget;
     stackedWidget->addWidget(mainPage);
     stackedWidget->addWidget(secondPage);
 
-    // Creating the objects for the window
+
     TopBar*             topBar = new TopBar(mainPage);
     Blinkers*           leftB = new Blinkers(mainPage, "left", "off");
     Blinkers*           rightB = new Blinkers(mainPage, "right", "off");
@@ -26,18 +24,18 @@ int main(int argc, char* argv[])
     BatteryWidget*      battery2 = new BatteryWidget(mainPage);
     ArrowSymbolWidget*  arrowSymbol = new ArrowSymbolWidget(mainPage, "zero");
     CameraWidget        *cameraWidget = new CameraWidget(secondPage);
-    //setCentralWidget(cameraWidget);
 
-    // Set a manager to handle keyboard events
+
     EventManager        eventManager(arrowSymbol, speedometer1, leftB, rightB, stackedWidget);
     app.installEventFilter(&eventManager);
 
-    // Create layouts
-    QVBoxLayout*        mainLayout = new QVBoxLayout();
-    QVBoxLayout*        mainPageLayout = new QVBoxLayout(); // Main vertical layout
-    QHBoxLayout*        mainContentLayout = new QHBoxLayout(); // Horizontal layout for speedometers
+
+    QVBoxLayout*        firstLayout = new QVBoxLayout();
+    QVBoxLayout*        secondLayout = new QVBoxLayout();
+    QVBoxLayout*        mainPageLayout = new QVBoxLayout();
+    QHBoxLayout*        mainContentLayout1 = new QHBoxLayout();
+    QHBoxLayout*        mainContentLayout2 = new QHBoxLayout();
     QHBoxLayout*        smallerLayout = new QHBoxLayout();
-    QVBoxLayout*        secondPageLayout = new QVBoxLayout(); // Main vertical layout
 
     arrowSymbol->setFixedSize(400, 400);
     app.installEventFilter(&eventManager);
@@ -46,32 +44,33 @@ int main(int argc, char* argv[])
     speedometer1->setFixedSize(400, 400);
     battery2->resize(900, 900);
     battery2->setFixedSize(400, 400);
+    cameraWidget->resize(1920, 1080);
+    cameraWidget->setFixedSize(700,500);
 
-
-    mainPageLayout->addWidget(topBar, 0, Qt::AlignCenter); // Add the TopBar widget at the top
-    mainPageLayout->addWidget(fanspeed, 0); // Add the TopBar widget at the top
+    mainPageLayout->addWidget(topBar, 0, Qt::AlignCenter);
+    mainPageLayout->addWidget(fanspeed, 0);
 
     // Adding objects to the second page
     // secondPageLayout->addWidget(battery2, 1);
     // secondPage->setLayout(secondPageLayout);
 
-    // Add the horizontal layout to the main vertical layout
-    mainContentLayout->addWidget(leftB, 1);
-    mainContentLayout->addWidget(speedometer1, 1);
-    mainContentLayout->addWidget(arrowSymbol, 1);
-    mainContentLayout->addWidget(battery2, 1);
-    mainContentLayout->addWidget(rightB, 1);
-    // Add Layout to the main Page
-    mainPageLayout->addLayout(mainContentLayout, 1);
-    // Set the layout for the main page
+    mainContentLayout1->addWidget(leftB, 1);
+    mainContentLayout1->addWidget(speedometer1, 1);
+    mainContentLayout1->addWidget(arrowSymbol, 1);
+    mainContentLayout1->addWidget(battery2, 1);
+    mainContentLayout1->addWidget(rightB, 1);
+    mainPageLayout->addLayout(mainContentLayout1, 1);
     mainPage->setLayout(mainPageLayout);
 
-    //Add a navigation tool for pages
+    mainContentLayout2->addWidget(leftB, 1);
+    mainContentLayout2->addWidget(cameraWidget, 1);
+    mainContentLayout2->addWidget(rightB, 1);
+    secondLayout->addLayout(mainContentLayout2, 1);
+    secondPage->setLayout(secondLayout);
     QToolBar*   toolBar = new QToolBar;
     QAction*    page1Action = toolBar->addAction("Main");
     QAction*    page2Action = toolBar->addAction("Second");
 
-    // Connect the labels to trigger the pages
     QObject::connect(page1Action, &QAction::triggered, [&]() {
         stackedWidget->setCurrentIndex(0);
     });
@@ -79,10 +78,10 @@ int main(int argc, char* argv[])
         stackedWidget->setCurrentIndex(1);
     });
 
-    mainLayout->addWidget(toolBar);
-    mainLayout->addWidget(stackedWidget);
+    firstLayout->addWidget(toolBar);
+    firstLayout->addWidget(stackedWidget);
 
-    mainLayout->addWidget(topBar, 0, Qt::AlignCenter);
+    firstLayout->addWidget(topBar, 0, Qt::AlignCenter);
 
     fanspeed->setFixedSize(150,150);
     cputemp->setFixedSize(150,150);
@@ -90,11 +89,11 @@ int main(int argc, char* argv[])
     smallerLayout->addWidget(fanspeed, 0);
     smallerLayout->addWidget(cputemp, 0);
     smallerLayout->setSpacing(0);
-    mainLayout->addLayout(smallerLayout, 0);
-    mainLayout->setAlignment(smallerLayout, Qt::AlignTop | Qt::AlignLeft);
+    firstLayout->addLayout(smallerLayout, 0);
+    firstLayout->setAlignment(smallerLayout, Qt::AlignTop | Qt::AlignLeft);
 
     // Set layout for the main window
-    mainWindow->setLayout(mainLayout);
+    mainWindow->setLayout(firstLayout);
     mainWindow->resize(1468, 768);
     mainWindow->setStyleSheet("background-color: #0D2126");
     mainWindow->grabGesture(Qt::SwipeGesture);
