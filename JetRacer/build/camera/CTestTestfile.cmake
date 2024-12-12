fc42bd23d250ca33d3ef6664463470c88bc12d67
Axel_ex <axel.chabrerie@gmail.com>
2024-12-12 14:36:05 +0000

    clean build dir

--- JetRacer/build/camera/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/jetpack/SEAME-Cluster-24-25/JetRacer/src/peripherals/camera
-# Build directory: /home/jetpack/SEAME-Cluster-24-25/JetRacer/build/camera
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
