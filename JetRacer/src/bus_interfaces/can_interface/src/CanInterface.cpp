88a1dac38736510bc83cc3b04b4baf98f5c31999
Axel_ex <axel.chabrerie@gmail.com>
2024-12-13 10:41:58 +0000

    update can interface

--- JetRacer/src/bus_interfaces/can_interface/src/CanInterface.cpp
+++ JetRacer/src/bus_interfaces/can_interface/src/CanInterface.cpp
@@ -9,10 +9,17 @@
 #include <thread>
 
 using sockcanpp::CanMessage;
+using namespace std::chrono_literals;
 
 CanInterface::CanInterface() : Node("can_interface")
 {
+    rclcpp::QoS qos(40);
+    qos.reliable();
+
     can_driver_ = std::make_shared<CanDriver>("can0", CAN_RAW);
+    can_service_ = this->create_service<custom_msgs::srv::CanService>(
+        "can_service", [this](const auto request, auto response)
+        { handleCanRequest(request, response); }, qos);
     publisher_ =
         this->create_publisher<custom_msgs::msg::CanFrame>("raw_CAN", 10);
     polling_thread_ = std::thread(&CanInterface::pollCanBus, this);
@@ -51,4 +58,48 @@ void CanInterface::pollCanBus()
             publisher_->publish(ros_msg);
         }
     }
+}
+
+void CanInterface::handleCanRequest(
+    const std::shared_ptr<custom_msgs::srv::CanService::Request> request,
+    std::shared_ptr<custom_msgs::srv::CanService::Response> response)
+{
+    response->set__success(true);
+
+    if (!request->write_data.empty())
+    {
+        can_frame frame;
+
+        frame.can_id = request->can_id;
+        frame.can_dlc = request->write_data.size();
+        std::copy(request->write_data.begin(), request->write_data.end(),
+                  frame.data);
+
+        try
+        {
+            can_driver_->sendMessage(frame);
+        }
+        catch (const std::exception& e)
+        {
+            response->set__success(false);
+            response->set__message(e.what());
+            RCLCPP_ERROR(this->get_logger(), "Fail writing data: %s", e.what());
+        }
+    }
+
+    if (request->read_request)
+    {
+        if (!can_driver_->waitForMessages(50ms))
+        {
+            response->set__success(false);
+            response->set__message("request timedout");
+            RCLCPP_ERROR(this->get_logger(),
+                         "Fail reading can bus, request timed out");
+        }
+
+        CanMessage received_msg = can_driver_->readMessage();
+        auto frame_data = received_msg.getFrameData();
+        std::copy(frame_data.begin(), frame_data.end(), response->read_data);
+    }
+    response->set__message("success");
 }
