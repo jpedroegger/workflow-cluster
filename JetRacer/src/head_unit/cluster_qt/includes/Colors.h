bd087c5023d88569610157fb3ce048dd8ffacd24
Xyckens <franciscommvieira@hotmail.com.com>
2025-01-29 12:58:40 +0000

    left things commented by mistake

--- JetRacer/src/head_unit/cluster_qt/includes/Colors.h
+++ JetRacer/src/head_unit/cluster_qt/includes/Colors.h
@@ -43,27 +43,27 @@ public:
     }
     int     counter;
 
-    //QColor  main_color_array[4] = {"#312244", "#9A031E", "#FF99C8", "#283618"};
-    //QColor  main_color = main_color_array[counter];
+    QColor  main_color_array[4] = {"#312244", "#9A031E", "#FF99C8", "#283618"};
+    QColor  main_color = main_color_array[counter];
 
-    //QColor  accent_color_array[4] = {"#4D194D", "#FB8B24", "#A9DEF9", "#BC6C25"};
-    //QColor  accent_color = accent_color_array[counter];
+    QColor  accent_color_array[4] = {"#4D194D", "#FB8B24", "#A9DEF9", "#BC6C25"};
+    QColor  accent_color = accent_color_array[counter];
 
-    //QColor  alphabet_color_array[4] = {"#006466", "#E36414", "#E4C1F9", "#FEFAE0"};
-    //QColor  alphabet_color = alphabet_color_array[counter];
+    QColor  alphabet_color_array[4] = {"#006466", "#E36414", "#E4C1F9", "#FEFAE0"};
+    QColor  alphabet_color = alphabet_color_array[counter];
 
-    //QString background_array[4] = {"background-color: #212F45", "background-color: #5F0F40", "background-color: #D0F4DE", "background-color: #DDA15E"};
-    //QString background = background_array[counter];
+    QString background_array[4] = {"background-color: #212F45", "background-color: #5F0F40", "background-color: #D0F4DE", "background-color: #DDA15E"};
+    QString background = background_array[counter];
 
     void    indent(void)
     {
         this->counter++;
         if (this->counter > 3)
             this->counter = 0;
-        //this->main_color = main_color_array[counter];
-        //this->accent_color = accent_color_array[counter];
-        //this->alphabet_color = alphabet_color_array[counter];
-        //this->background = background_array[counter];
+        this->main_color = main_color_array[counter];
+        this->accent_color = accent_color_array[counter];
+        this->alphabet_color = alphabet_color_array[counter];
+        this->background = background_array[counter];
         std::ofstream f("color.txt");
 
         // Write to the file
