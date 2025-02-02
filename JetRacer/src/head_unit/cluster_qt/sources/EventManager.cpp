5f7e4d1c0dbf7ebd028027c3bb7fdcafbf90afd5
jegger <jpedroegger@gmail.com>
2025-02-02 20:26:51 +0000

    Refactoring EventManager class to follow our clang style

--- JetRacer/src/head_unit/cluster_qt/sources/EventManager.cpp
+++ JetRacer/src/head_unit/cluster_qt/sources/EventManager.cpp
@@ -7,43 +7,43 @@ EventManager::EventManager(
     Blinkers* left_blinker, Blinkers* right_blinker, Blinkers* left_blinker2,
     Blinkers* right_blinker2, StatsWidget* stats, FanSpeedWidget* fan,
     FanSpeedWidget* fan2, CPUTempWidget* cpu, CPUTempWidget* cpu2, TopBar* top,
-    TopBar* top2, QStackedWidget* stackedWidget, QWidget* mainWindow,
+    TopBar* top2, QStackedWidget* stacked_widget, QWidget* main_window,
     std::shared_ptr<RosNode> ros_node)
     : arrows(arrow), py_speed(py_speed), py_battery(py_battery),
       py_batspeed(py_batspeed), left_blinker(left_blinker),
       right_blinker(right_blinker), left_blinker2(left_blinker2),
       right_blinker2(right_blinker2), stats(stats), fan(fan), fan2(fan2),
-      cpu(cpu), cpu2(cpu2), top(top), top2(top2), stackedWidget(stackedWidget),
-      mainWindow(mainWindow), node(ros_node)
+      cpu(cpu), cpu2(cpu2), top(top), top2(top2), stacked_widget(stacked_widget),
+      main_window(main_window), node(ros_node)
 {
     color1 = Color();
-    updateTimer = new QTimer(this);
-    connect(updateTimer, &QTimer::timeout, this, &EventManager::updateScreen);
+    update_timer = new QTimer(this);
+    connect(update_timer, &QTimer::timeout, this, &EventManager::updateScreen);
     executor.add_node(node);
-    updateTimer->start(REFRESH_RATE_MS); // Every 50ms => 20FPS
+    update_timer->start(REFRESH_RATE_MS); // Every 50ms => 20FPS
 }
 
 bool EventManager::eventFilter(QObject* obj, QEvent* event)
 {
     if (event->type() == QEvent::MouseButtonPress) {
-        QMouseEvent* mousePos = static_cast<QMouseEvent*>(event);
-        QPointF pressPosition = mousePos->position();
+        auto mouse_event = static_cast<QMouseEvent*>(event);
+        QPointF press_position = mouse_event->position();
 
-        mousePosition = pressPosition;
+        mouse_position = press_position;
         return true;
     }
     else if (event->type() == QEvent::MouseButtonRelease) {
-        QMouseEvent* mousePos = static_cast<QMouseEvent*>(event);
-        QPointF releasePosition = mousePos->position();
-        if (swipe(releasePosition))
+        auto mouse_event = static_cast<QMouseEvent*>(event);
+        QPointF release_position = mouse_event->position();
+        if (swipe(release_position))
             return true;
     }
     else if (event->type() == QEvent::KeyRelease)
     {
-        QKeyEvent* keyEvent = static_cast<QKeyEvent*>(event);
+        auto key_event = static_cast<QKeyEvent*>(event);
         
-        if (!keyEvent->isAutoRepeat())
-            activateButton(keyEvent->key());
+        if (!key_event->isAutoRepeat())
+            activateButton(key_event->key());
         return true;
     }
     return QObject::eventFilter(obj, event);
@@ -67,17 +67,17 @@ void    EventManager::activateButton(int key)
 void EventManager::updateScreen()
 {
     executor.spin_some(
-        std::chrono::milliseconds(10)); // after this line, values are updated.
+        std::chrono::milliseconds(SCREEN_UPDATE_RATE)); // after this line, values are updated.
     py_speed->setCurrentSpeed(node->getSpeed());
     py_battery->setCurrentLevel(node->getBattery());
     py_batspeed->setCurrentLevel(node->getBattery());
     py_batspeed->setCurrentSpeed(node->getSpeed());
     updateBlinkers();
 
-    stats->setDistance(4533);
-    stats->setAverage(56);
-    stats->setConsumed(78);
-    stats->setObstacles(2);
+    stats->setDistance(STATS_DISTANCE);
+    stats->setAverage(STATS_AVERAGE);
+    stats->setConsumed(STATS_CONSUMED);
+    stats->setObstacles(STATS_OBSTACLES);
     stats->update();
 }
 
@@ -135,7 +135,7 @@ void EventManager::changeColors()
     fan2->changeColor(color1.counter);
     cpu->changeColor(color1.counter);
     cpu2->changeColor(color1.counter);
-    mainWindow->setStyleSheet(color1.background_array[color1.counter]);
+    main_window->setStyleSheet(color1.background_array[color1.counter]);
 }
 
 void EventManager::changeUnits()
@@ -147,20 +147,20 @@ void EventManager::changeUnits()
     cpu2->changeUnits();
 }
 
-bool    EventManager::swipe(QPointF releasePosition)
+bool    EventManager::swipe(QPointF release_position)
 {
-    bool swipeRange = qFabs(mousePosition.rx() - releasePosition.rx()) > 100;
-    bool verticalRange = qFabs(mousePosition.ry() - releasePosition.ry()) < 200;
+    bool swipe_range = qFabs(mouse_position.rx() - release_position.rx()) > MIN_SWIPE_WIDTH;
+    bool vertical_range = qFabs(mouse_position.ry() - release_position.ry()) < MAX_SWIPE_HEIGHT;
 
-    if (mousePosition.rx() < releasePosition.rx() && swipeRange && verticalRange ) {
-        int prevIndex = std::max(0, stackedWidget->currentIndex() - 1);
-        stackedWidget->setCurrentIndex(prevIndex);
+    if (mouse_position.rx() < release_position.rx() && swipe_range && vertical_range ) {
+        int previous_index = std::max(0, stacked_widget->currentIndex() - 1);
+        stacked_widget->setCurrentIndex(previous_index);
 
         return true;
     }
-    else if (mousePosition.rx() > releasePosition.rx() && swipeRange && verticalRange ) {
-        int nextIndex = std::min(stackedWidget->count() - 1, stackedWidget->currentIndex() + 1);
-        stackedWidget->setCurrentIndex(nextIndex);
+    else if (mouse_position.rx() > release_position.rx() && swipe_range && vertical_range ) {
+        int next_index = std::min(stacked_widget->count() - 1, stacked_widget->currentIndex() + 1);
+        stacked_widget->setCurrentIndex(next_index);
 
         return true;
     }
@@ -168,6 +168,6 @@ bool    EventManager::swipe(QPointF releasePosition)
 }
 
 
-QStackedWidget* EventManager::getStackedWidget() { return stackedWidget; }
+QStackedWidget* EventManager::getStackedWidget() { return stacked_widget; }
 
 EventManager::~EventManager() {}
