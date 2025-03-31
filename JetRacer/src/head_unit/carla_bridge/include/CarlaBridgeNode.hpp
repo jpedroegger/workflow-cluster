f6f699869399155871998c9cb962367c580d6430
jegger <jpedroegger@gmail.com>
2025-03-31 14:02:14 +0100

    Adding CARLA-Ros2 bridge

---
+++ JetRacer/src/head_unit/carla_bridge/include/CarlaBridgeNode.hpp
@@ -0,0 +1,18 @@
+#pragma once
+
+#include <rclcpp/rclcpp.hpp>
+#include <geometry_msgs/msg/twist.hpp>
+#include <carla_msgs/msg/carla_ego_vehicle_control.hpp>
+
+
+class CarlaBridgeNode : public rclcpp::Node
+{
+public:
+    CarlaBridgeNode();
+
+private:
+    void twistCallback(const geometry_msgs::msg::Twist::SharedPtr twist_msg);
+
+    rclcpp::Subscription<geometry_msgs::msg::Twist>::SharedPtr twist_subscriber_;
+    rclcpp::Publisher<carla_msgs::msg::CarlaEgoVehicleControl>::SharedPtr carla_control_publisher_;
+};
---
+++ JetRacer/src/head_unit/carla_bridge/src/CarlaBridgeNode.cpp
@@ -0,0 +1,22 @@
+#include "CarlaBridgeNode.hpp"
+
+CarlaBridgeNode::CarlaBridgeNode() : rclcpp::Node("carla_bridge_node")
+{
+    twist_subscriber_ = this->create_subscription<geometry_msgs::msg::Twist>(
+        "cmd_vel", 10,
+        std::bind(&CarlaBridgeNode::twistCallback, this, std::placeholders::_1));
+
+    carla_control_publisher_ = this->create_publisher<carla_msgs::msg::CarlaEgoVehicleControl>(
+        "/carla/ego_vehicle/vehicle_control_cmd", 10);
+}
+
+void CarlaBridgeNode::twistCallback(const geometry_msgs::msg::Twist::SharedPtr twist_msg)
+{
+    auto carla_control_msg = carla_msgs::msg::CarlaEgoVehicleControl();
+    
+    carla_control_msg.throttle = twist_msg->linear.x;
+    carla_control_msg.brake = (twist_msg->linear.x < 0) ? -twist_msg->linear.x : 0.0;
+    carla_control_msg.steer = twist_msg->angular.z;
+
+    carla_control_publisher_->publish(carla_control_msg);
+}
